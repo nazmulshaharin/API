c32a46d81b74674836ce40d6234fb0558470eadb
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2022-12-19 21:07:06 +0600

    jdejdjdjjd

--- js/api.js
+++ js/api.js
@@ -4,3 +4,21 @@ function loadData(){
   .then(response => response.json())
   .then(json => console.log(json))
 }
+
+//
+function loadUsers(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => displayUser(data))
+}
+//
+function loadPosts(){
+  fetch('https://jsonplaceholder.typicode.com/posts')
+  .then(resp => resp.json())
+  .then(post => console.log(post))
+}
+//loadPosts(); এটার মাধ্যমে ডাটা অটোমেটিক লোড হয়ে যাবে
+
+function displayUser(data){
+  console.log(data);
+}
