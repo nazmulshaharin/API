1020ecd2c96c7a6d438e986f185cb6cadf1acacf
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2022-12-19 20:09:26 +0600

    jjsjsjjs

--- js/index.js
+++ js/index.js
@@ -1,4 +1,19 @@
 const user = {id: 11, name: 'Amir khan', job: 'actor'};
 const stringified = JSON.stringify(user);
 console.log(user);
-console.log(stringified)
+console.log(stringified);
+
+//another
+const shop = {
+  name: 'alia store',
+  address: 'ranbir road',
+  profit: 25000,
+  products: ['laptop', 'mobile', 'pepsi'],
+  isExpensive: false
+}
+const shopStrigifier = JSON.stringify(shop);
+//console.log(shop);
+console.log(shopStrigifier);
+const converted = JSON.parse(shopStrigifier);
+console.log(converted);
+console.log(converted.products);
