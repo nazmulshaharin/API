fc4383406c34edb3ef10c025d0d7a7eb8922a77b
nazmulshaharin <smnazmulhossainshaharin@gmail.com>
2022-12-21 00:28:37 +0600

    jjsjsjsj

--- js/api.js
+++ js/api.js
@@ -15,10 +15,36 @@ function loadUsers(){
 function loadPosts(){
   fetch('https://jsonplaceholder.typicode.com/posts')
   .then(resp => resp.json())
-  .then(post => console.log(post))
+  //.then(post => console.log(post))
+  //or
+  .then(post => displayPost(post))
 }
+
+function displayPost(post){
+console.log(post)
+/* const ul = document.getElementById('post');
+  for(const status of post){
+//console.log(status);
+const li = document.createElement('li');
+li.innerText = `TILTLE: ${status.title}`;
+ul.appendChild(li);
+}   
+} */
+}
+
+//
 //loadPosts(); এটার মাধ্যমে ডাটা অটোমেটিক লোড হয়ে যাবে
 
-function displayUser(data){
-  console.log(data);
-}
+ function displayUser(data){
+ // console.log(data);
+ //const ul = document.getElementById('users');
+ for (const user of data){
+  console.log(user);
+//
+ const li = document.createElement('li');
+//li.innerText = 'ami';
+//li.innerText = user.name;
+li.innerText = `name: ${user.name} Email: ${user.email}`;
+ul.appendChild(li);
+ } 
+ }
